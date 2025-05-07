b03058dee3afd669124edda5953e6ec0adb27cf2
Razi Berg <razi.berg@gmail.com>
2025-05-07 23:42:23 +0100

    first working prototype of pi to arduino comms

--- pi_python_comms_test.py
+++ pi_python_comms_test.py
@@ -1,12 +1,10 @@
 import serial
 import time
 
-ser = serial.Serial("/dev/serial10", 9600)
+ser = serial.Serial("/dev/serial0", 9600)
 time.sleep(2);
 
 ser.write(b'Hello from Raspberry Pi\n')
-while ser.in_waiting:
-	print(ser.readline().decode())
-
+print("message sent")
 
 ser.close()
